603c9ff263891929a5d486400efe442b5e4c1f97
CosimoA <cosimo.archip@gmail.com>
2024-09-17 15:09:39 +0200

    FEAT: create and import text headline component

---
+++ progetto1 - 2 ComponentiCustom/lib/components/text_headline.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TextHeadline extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Flutter2Start");
+  }
+}
--- progetto1 - 2 ComponentiCustom/lib/pages/home_page.dart
+++ progetto1 - 2 ComponentiCustom/lib/pages/home_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:progetto_1/components/text_headline.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: TextHeadline(),
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
