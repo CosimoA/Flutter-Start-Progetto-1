1972eb62a880f1619d1e548f9ce738f6be3ce102
CosimoA <cosimo.archip@gmail.com>
2024-09-17 15:34:49 +0200

    FEAT: passare dati su homepage da app.dart

--- progetto1 - 2 ComponentiCustom/lib/pages/home_page.dart
+++ progetto1 - 2 ComponentiCustom/lib/pages/home_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:progetto_1/components/text_headline.dart';
 
 class HomePage extends StatelessWidget {
+  final int value;
+  const HomePage(this.value);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,6 +13,7 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
+      body: Center(child: Text("Persone in lista d'attesa: $value")),
     );
   }
 }
